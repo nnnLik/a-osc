e14913f42c51235d0e796b2244d247885e2729d3
nnnLik <rslmakhmudov@gmail.com>
2024-07-30 17:55:37 +0300

    add loging file

--- main.py
+++ main.py
@@ -7,7 +7,11 @@ import mysql.connector
 
 logging.basicConfig(
     level=logging.INFO,
-    format='%(asctime)s - %(levelname)s - %(message)s'
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    handlers=[
+        logging.StreamHandler(),
+        logging.FileHandler(f'migration_{int(time.time())}.log', mode='w')
+    ]
 )
 logger = logging.getLogger()
 
